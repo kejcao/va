e3ef7a08c703a7f0d3d471b7916fe317b3a61ca0
Kevin Cao <kjcao@proton.me>
2024-02-21 16:46:54 -0500

    Small quality of life changes

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ from llama_cpp import Llama
 METHOD = 'API'  # either API or LOCAL
 
 (RUNNING, STOPED, REQUEST_STOP) = range(3)
-llm_context = """\
-Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately. Bob always responds in less than 32 words.
+llm_context = ''
+if METHOD == 'LOCAL':
+    llm_context = """\
+    Transcript of a dialog, where User interacts with an Assistant named Bob. Bob always responds in less than 32 words.
 
-User: Hello, Bob.
-Bob: Hello. How may I help you today?
-User: Please tell me the largest city in Europe.
-Bob: Sure. The largest city in Europe is Moscow, the capital of Russia.
-"""
+    User: hello, bob
+    Bob: Hi!
+    User: please tell me the largest city in europe
+    Bob: Moscow, the capital of Russia, is the largest city in Europe.
+    """
 llm_running = STOPED
 
 llm = None
@@ -29,10 +31,23 @@ if METHOD == 'LOCAL':
 def stream(prompt):
     if METHOD == 'API':
         for token in replicate.stream(
-            'meta/llama-2-7b',
-            input={'prompt': prompt, 'temperature': 0.5, 'stop_sequences': ['User:']},
+            'meta/llama-2-7b-chat',
+            input={
+                'prompt': prompt,
+                'system_prompt': 'You are an AI assistant named Bob. Bob always adheres to the user\'s requests but responds in less than 32 words.',
+                'temperature': 0.8,
+            },
+            # input={'prompt': prompt, 'temperature': 0.5, 'stop_sequences': '<User:>'}
         ):
-            yield str(token)
+            token = str(token)
+
+            if not token.strip():
+                continue
+
+            l = token.split(' ')
+            for t in l[:-1]:
+                yield str(t + ' ')
+            yield l[-1] + (' ' if token[-1] == ' ' else '')
     elif METHOD == 'LOCAL':
         for d in llm(prompt, max_tokens=32, stop=['User:'], echo=False, stream=True):
             yield d['choices'][0]['text']
@@ -41,16 +56,15 @@ def stream(prompt):
 def prompt_llm(prompt, quiet=False):
     global llm_context, llm_running
 
-    print('BOT | ', end='', flush=True)
+    print('Bob | ', end='', flush=True)
 
-    llm_context += f'User: {prompt}\n'
+    llm_context += (
+        f'User: {prompt}\nBob: ' if METHOD == 'local' else f'[INST]{prompt}[/INST]\n'
+    )
     reply = ''
     start = time.time()
 
     for token in stream(llm_context):
-        if not token.strip():
-            continue
-
         if llm_running == REQUEST_STOP:
             break
 
@@ -60,7 +74,7 @@ def prompt_llm(prompt, quiet=False):
             break
 
         # polling sort of bad :/ maybe killable threads?
-        while time.time() - start < 0.1:
+        while time.time() - start < 0.17:
             time.sleep(0.01)
             if llm_running == REQUEST_STOP:
                 break
@@ -78,7 +92,8 @@ def callback(transcribed_text, activity):
     global llm_running
 
     def update_gui(**kwargs):
-        gui.print_user_input(f'{activity[-1]:.1f}> ', transcribed_text, **kwargs)
+        gui.print_user_input('> ', transcribed_text, **kwargs)
+        # gui.print_user_input(f'{activity[-1]:.1f}> ', transcribed_text, **kwargs)
 
     # so while the chatbot was responding, the user interrupted
     if llm_running == RUNNING and transcribed_text:
