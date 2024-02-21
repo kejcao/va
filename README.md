b5a2fee50f9628d2d5c638da3bd56ae2ed3c7864
Kevin Cao <kjcao@proton.me>
2024-02-21 16:00:24 -0500

    Add ability to choose between API or local LLM

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Voice Assistant
 
-My attempt to make a voice assistant. Inspired by [LAION](https://github.com/LAION-AI/natural_voice_assistant/), but I couldn't get their code to work on my CPU.
+My attempt to make a voice assistant. Inspired by [LAION's BUD-E](https://github.com/LAION-AI/natural_voice_assistant/), but I couldn't get their code to work on my CPU.
 
-Code assumes you have [ollama](https://github.com/ollama/ollama) running in the background. To the voice assistant run:
+It uses [Nvidia](https://catalog.ngc.nvidia.com/orgs/nvidia/teams/nemo/models/stt_en_fastconformer_hybrid_large_streaming_80ms)'s STT En FastConformer Hybrid Transducer-CTC Large Streaming 80ms for local STT. There are two options for the LLM backend:
+
+- Use the Replicate API. In which case, make sure you export your API key as an environmental variable. [More details here](https://replicate.com/docs/get-started/python).
+- Use the Python bindings for `llama.cpp` to run LLM weights in the `models` directory locally—I use Google's Gemma-7b, but you could use Mixtral if you have the hardware or something else like Microsoft's Phi.
+
+## To run
+
+Make sure you setup one of the LLM backends. To actually activate the voice assistant, install the dependences and run `main.py`:
 
 ```
 $ pip install -r requirements.txt
 $ python3 main.py
 ```
-
-Internally, it uses:
-
-- [Nvidia](https://catalog.ngc.nvidia.com/orgs/nvidia/teams/nemo/models/stt_en_fastconformer_hybrid_large_streaming_80ms)'s STT En FastConformer Hybrid Transducer-CTC Large Streaming 80ms for STT.
-- Replicate API to run the LLM, so make sure you have an API key environmental variable: [more details](https://replicate.com/docs/get-started/python).
--- main.py
+++ main.py
@@ -6,40 +6,72 @@ import replicate
 import time
 import json
 import threading
+from llama_cpp import Llama
 
-llm_context = ''
-llm_running = False
+METHOD = 'API'  # either API or LOCAL
+
+(RUNNING, STOPED, REQUEST_STOP) = range(3)
+llm_context = """\
+Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately. Bob always responds in less than 32 words.
+
+User: Hello, Bob.
+Bob: Hello. How may I help you today?
+User: Please tell me the largest city in Europe.
+Bob: Sure. The largest city in Europe is Moscow, the capital of Russia.
+"""
+llm_running = STOPED
+
+llm = None
+if METHOD == 'LOCAL':
+    llm = Llama(model_path='./models/gemma-7b-it-Q4_K_M.gguf', seed=67)
+
+
+def stream(prompt):
+    if METHOD == 'API':
+        for token in replicate.stream(
+            'meta/llama-2-7b',
+            input={'prompt': prompt, 'temperature': 0.5, 'stop_sequences': ['User:']},
+        ):
+            yield str(token)
+    elif METHOD == 'LOCAL':
+        for d in llm(prompt, max_tokens=32, stop=['User:'], echo=False, stream=True):
+            yield d['choices'][0]['text']
 
 
 def prompt_llm(prompt, quiet=False):
     global llm_context, llm_running
 
-    prompt = f'[INST] {prompt} [/INST]\n'
-    llm_context += prompt
-
     print('BOT | ', end='', flush=True)
 
+    llm_context += f'User: {prompt}\n'
     reply = ''
-    for event in replicate.stream(
-        'meta/llama-2-7b-chat',
-        input={
-            'prompt': llm_context,
-            # 'system_prompt': 'You are an AI assistant. Always respond as concisely as possible. Neglect to mention any unnecessary information not pertaining to the user input. Use as little words as possible.',
-            'system_prompt': 'You are an AI assistant. Use as little words as possible. Your responses should not exceed 30 words.',
-            'temperature': 0.1,
-        },
-    ):
-        tok = str(event)
-        reply += tok
-        if tok.strip():
-            print(tok, flush=True, end='')
+    start = time.time()
 
+    for token in stream(llm_context):
+        if not token.strip():
+            continue
+
+        if llm_running == REQUEST_STOP:
+            break
+
+        reply += token
+        print(token, flush=True, end='')
+        if reply[:7].lower().strip() == 'goodbye':
+            break
+
+        # polling sort of bad :/ maybe killable threads?
+        while time.time() - start < 0.1:
+            time.sleep(0.01)
+            if llm_running == REQUEST_STOP:
+                break
+        start = time.time()
+
+    # preamble, to cleanly exit
     print()
     print()
-
-    llm_context += reply.strip() + '\n'
-    llm_running = False
     transcribe.reset_stt()
+    llm_context += reply.strip() + '\n'
+    llm_running = STOPED
 
 
 def callback(transcribed_text, activity):
@@ -48,20 +80,24 @@ def callback(transcribed_text, activity):
     def update_gui(**kwargs):
         gui.print_user_input(f'{activity[-1]:.1f}> ', transcribed_text, **kwargs)
 
+    # so while the chatbot was responding, the user interrupted
+    if llm_running == RUNNING and transcribed_text:
+        llm_running = REQUEST_STOP
+        return
 
-    if transcribed_text and len(activity) > 2 and all(x < 0.2 for x in activity[-2:]):
-        if not llm_running and transcribed_text:
-            llm_running = True
+    if llm_running == STOPED:
+        update_gui()
+        if (
+            transcribed_text
+            and len(activity) > 2
+            and all(x < 0.2 for x in activity[-2:])
+        ):
+            llm_running = RUNNING
             update_gui(erase=False, grayout=True)
             threading.Thread(
                 target=prompt_llm, args=(transcribed_text,), daemon=True
             ).start()
             transcribe.reset_stt()
-
-        # transcribe.reset_stt()
-
-    if not llm_running:
-        update_gui()
 
 
 transcribe.start(callback)
